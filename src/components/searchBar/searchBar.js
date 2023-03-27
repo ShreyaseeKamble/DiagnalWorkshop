8d12dbef8a0c4030dba3bf8f2b07ffcbb608db09
ShreyaseeKamble <42696054+ShreyaseeKamble@users.noreply.github.com>
2023-03-27 23:47:58 +0530

    back button

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -3,14 +3,14 @@ import backButton from '../../assets/Back.png';
 import searchButton from '../../assets/search.png';
 import './searchBar.css';
 
-const SearchBar = ({searchText, handleInputBlur, handleInputChange}) => {
+const SearchBar = ({searchText, handleInputBlur, handleInputChange, handleBackButtonClicked}) => {
   const handleBlur = () => {
     handleInputBlur();
   }
 
   return (
     <div className="search_bar">
-      <img src={backButton} alt='back button' width="30" height="30"></img>
+      <img src={backButton} alt='back button' width="30" height="30" onClick={handleBackButtonClicked}></img>
       <input className="input_box" value={searchText} onBlur={(event) => handleBlur(event.target.value)} onChange={(event) => handleInputChange(event.target.value)}></input>
       <img src={searchButton} alt='search button' width="30" height="30" onClick={handleBlur}></img>
     </div>
